525b8448a92c7c7c6e1ca9d681a607a02dda8208
Carlos Arboleda <carlosarboleda@Carloss-MacBook-Air-2.local>
2020-03-28 18:49:35 -0500

    cambios al bot ya tiene un menusito que no hace nada pero alli va, no me ha servido el bot.on

--- telegram/index.js
+++ telegram/index.js
@@ -1,12 +1,43 @@
-var TelegramBot=require('node-telegram-bot-api');
+//const app=require('express')();
+//app.listen(9110);
+//app.get("/",(req, res)=>{res.send("Hello")})
+
+const TelegramBot=require('node-telegram-bot-api');
 var token='1122418522:AAHL4WzNb7DE7fBPora-HOm_V5lJoMTEqF8';
 
-var bot = new TelegramBot(token,{polling:true});
+const bot = new TelegramBot(token,{polling:true});
 
-bot.onText(/\/echo (.+)/,function(msg,match){
 
-    var chatId=msg.chat.id;
-    var echo=match[1];
-    bot.sendMessage(chatId,echo);
+bot.onText(/\/Ayuda/, function onLoveText(msg) {
+    const opts = {
+      reply_to_message_id: msg.message_id,
+      reply_markup: JSON.stringify({
+        keyboard: [
+          ['Ayudame con los sintomas.'],
+          ['Mi clinica mas cercana.'],
+          ['Policia.'],
+        ]
+      })
+    };
+    bot.sendMessage(msg.chat.id, 'En que me puede ayudar?', opts);
+  });
+  
+ bot.on('text', (msg)=>{
+    let text=msg.text.toLocaleLowerCase();
 
-});
+    if(text === 'Hola buenas tardes'){
+        bot.sendMessage(msg.from.id, "Buenas tardes en que le puede ayudar su bot de confianza")
+    }
+    if(text === 'Hola buenos dias'){
+        bot.sendMessage(msg.from.id, "Buenos dias en que le puede ayudar su bot de confianza")
+    }
+    if(text === 'Tengo una duda'){
+        bot.sendMessage(msg.from.id, "Que duda tiene")
+    }
+    if(text === 'Tengo Miedo'){
+        bot.sendMessage(msg.from.id, "No tenga miedo yo lo cuidare")
+    }
+    if(text === 'Me llamo jan'){
+        bot.sendMessage(msg.from.id, "Hola jan esto es una prueba att caliche ")
+    }    
+ });
